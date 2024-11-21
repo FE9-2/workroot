50746ed279b460fdf219b2d3aad10c100023c82d
yyezzzy <t47047877@gmail.com>
2024-11-21 13:13:59 +0900

    feat: body 폰트를 Gothic A1로 변경하고 관련 클래스 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/globals.css",
   ],
   theme: {
     extend: {
@@ -22,6 +23,7 @@ const config: Config = {
           "Apple SD Gothic Neo",
           "sans-serif",
         ],
+        gothic: ["Gothic A1", "sans-serif"],
       },
     },
   },
