7ea23f6371d516ae433c542e00fcd97cd0509f3a
hongggy <hongggy@gmail.com>
2024-11-21 17:59:54 +0900

    feat: input ui 스토리북 연결

---
+++ src/app/components/input/CommonInput.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { ChangeEvent, InputHTMLAttributes, useState } from "react";
+import { LuEye, LuEyeOff } from "react-icons/lu";
+
+interface CommonInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  color: string;
+  errors?: string;
+}
+
+interface InputProp {
+  type: string;
+  name?: string;
+  register?: (name: string) => { onChange: (e: ChangeEvent<HTMLInputElement>) => void; onBlur: () => void; ref: any };
+  errors?: boolean;
+  rules?: boolean;
+  size?: string;
+  value?: string;
+  placeholder?: string;
+  className?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CommonInput = (props: InputProp) => {
+  const [eyeOn, setEyeOn] = useState(false);
+  const toggleType = () => {
+    setEyeOn(!eyeOn);
+  };
+  const inputType = props.type === "password" && eyeOn ? "text" : props.type;
+
+  return (
+    <>
+      <label className="hidden">{props.name}</label>
+      <input
+        id={props.name}
+        type={inputType}
+        placeholder={props.placeholder}
+        value={props.value}
+        className={`rounded-lg border-[0.5px] p-[14px] font-normal ${props.className}`}
+        onChange={props.onChange}
+      />
+      {props.type === "password" && (
+        <div onClick={toggleType} className="cursor-pointer">
+          {eyeOn ? <LuEye /> : <LuEyeOff />}
+        </div>
+      )}
+      {props.errors && <p className="text-sm text-red-500">{props.errors}</p>}
+    </>
+  );
+};
+
+export default CommonInput;
