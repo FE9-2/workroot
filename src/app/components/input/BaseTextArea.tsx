6548e63fe9f9a12b9a3081c28c31a83b13632dda
hongggy <hongggy@gmail.com>
2024-11-22 14:54:33 +0900

    refactor: 스크롤바 추가를 위한 textarea 수정

--- src/app/components/input/BaseTextArea.tsx
+++ src/app/components/input/BaseTextArea.tsx
@@ -2,32 +2,33 @@ import { BaseTextAreaProps } from "@/types/textInput";
 
 const BaseTextArea = (props: BaseTextAreaProps) => {
   // 공통 스타일
-  const baseStyle = "resize-none rounded-lg focus:outline-none";
-  const sizingStyle = "h-[124px] w-[327px] p-[14px] lg:h-[196px] lg:w-[640px] lg:py-[18px]";
-  const typographyStyle =
+  const baseStyle = "resize-none focus:outline-none h-full w-full";
+  const textStyle =
     "text-black-400 placeholder:text-grayscale-400 text-sm font-normal leading-[26px] lg:text-base lg:leading-8";
 
   // variant에 따른 배경색 & border 스타일
   const variantStyle =
     props.variant === "white"
-      ? "bg-background-200 border border-transparent hover:border-line-200 focus:border-grayscale-400"
-      : "bg-transparent border-[0.5px] border-grayscale-200 caret-primary-orange-300 hover:border-grayscale-300 focus:border-primary-orange-300";
+      ? "bg-background-200 border border-transparent hover:border-line-200 [&:has(textarea:focus)]:border-grayscale-400"
+      : "bg-transparent border-[0.5px] border-grayscale-200 caret-primary-orange-300 hover:border-grayscale-300 [&:has(textarea:focus)]:border-primary-orange-300";
+
+  const textAreaBgStyle = props.variant === "white" ? "bg-background-200" : "bg-transparent";
+  const textareaStyle = `${baseStyle} ${textAreaBgStyle} ${textStyle} ${props.className || ""}`;
 
   const errorStyle = props.errorMessage ? "border-state-error" : "";
-
-  const combinedStyle = `${baseStyle} ${sizingStyle} ${typographyStyle} ${errorStyle} ${variantStyle} ${props.className || ""}`;
+  const wrapperStyle = `group p-[14px] lg:py-[18px] relative rounded-lg w-[327px] lg:w-[640px] h-[132px] lg:h-[160px] ${variantStyle} ${errorStyle}`;
 
   return (
     <>
       <label className="hidden">{props.name}</label>
-      <span className="relative">
-        <textarea id={props.name} placeholder={props.placeholder} disabled={props.disabled} className={combinedStyle} />
+      <div className={wrapperStyle}>
+        <textarea id={props.name} placeholder={props.placeholder} disabled={props.disabled} className={textareaStyle} />
         {props.errorMessage && (
           <span className="absolute -bottom-[22px] right-0 text-[13px] text-sm font-medium leading-[22px] text-state-error lg:text-base lg:leading-[26px]">
             {props.errorMessage}
           </span>
         )}
-      </span>
+      </div>
     </>
   );
 };
