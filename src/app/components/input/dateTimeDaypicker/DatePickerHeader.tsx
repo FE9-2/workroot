0f03a296f18cf24494a01c29e2da833837ef086c
hongggy <hongggy@gmail.com>
2024-11-27 18:28:17 +0900

    feat: date picker UI

--- src/app/components/input/dateTimeDaypicker/DatePickerHeader.tsx
+++ src/app/components/input/dateTimeDaypicker/DatePickerHeader.tsx
@@ -1,5 +1,3 @@
-import { MouseEvent } from "react";
-import { IoIosClose } from "react-icons/io";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 interface DatePickerHeaderProps {
@@ -8,7 +6,6 @@ interface DatePickerHeaderProps {
   increaseMonth: () => void;
   prevMonthButtonDisabled: boolean;
   nextMonthButtonDisabled: boolean;
-  handleOpenDropdown: () => void;
 }
 
 const DatePickerHeader = ({
@@ -17,14 +14,8 @@ const DatePickerHeader = ({
   increaseMonth,
   prevMonthButtonDisabled,
   nextMonthButtonDisabled,
-  handleOpenDropdown,
 }: DatePickerHeaderProps) => {
   const iconStyle = "size-6 lg:size-9 text-gray-200";
-  const handleClickClose = (e: MouseEvent<HTMLButtonElement>) => {
-    // e.stopPropagation();
-    handleOpenDropdown();
-    console.log("close");
-  };
   return (
     <div className="flex flex-col gap-2">
       <div className="lg:h-15 z-20 flex h-12 items-center justify-center text-sm font-semibold leading-6 lg:text-lg lg:font-normal lg:leading-[26px]">
@@ -40,11 +31,7 @@ const DatePickerHeader = ({
         <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
           <MdKeyboardArrowRight className={iconStyle} />
         </button>
-        {/* 닫기 버튼 */}
       </div>
-      <button type="button" onClick={handleClickClose} className="absolute left-[14px] top-5 cursor-pointer lg:top-6">
-        <IoIosClose className="size-6 text-black-100 lg:size-9" />
-      </button>
     </div>
   );
 };
--- src/app/components/input/dateTimeDaypicker/DatePickerInput.tsx
+++ src/app/components/input/dateTimeDaypicker/DatePickerInput.tsx
@@ -4,7 +4,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/locale";
 import { useState } from "react";
-import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { IoIosClose, IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 import { BsCalendar4 } from "react-icons/bs";
 import { useDropdownOpen } from "@/hooks/useDropdownOpen";
 import { useFormContext } from "react-hook-form";
@@ -21,20 +21,24 @@ const DatePickerInput = () => {
   const iconStyle = "text-black-400 size-9";
   const arrowIcon = isOpen ? <IoMdArrowDropup className={iconStyle} /> : <IoMdArrowDropdown className={iconStyle} />;
 
+  const formatDate = (date: Date) => {
+    return `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, "0")}/${date.getDate().toString().padStart(2, "0")}`;
+  };
+
   const handleChange = (update: [Date | null, Date | null]) => {
     const [start, end] = update;
 
     // 시작일만 선택된 경우
     if (start && !end) {
       setDateRange([start, undefined]);
-      setValue("datepicker", `${start.toLocaleDateString("ko-KR")} ~`);
+      setValue("datepicker", `${formatDate(start)} ~`);
       return;
     }
 
     // 시작일과 종료일이 모두 선택된 경우
     if (start && end && end > start) {
       setDateRange([start, end]);
-      setValue("datepicker", `${start.toLocaleDateString("ko-KR")} ~ ${end.toLocaleDateString("ko-KR")}`);
+      setValue("datepicker", `${formatDate(start)} ~ ${formatDate(end)}`);
       handleOpenDropdown();
     }
   };
@@ -52,23 +56,36 @@ const DatePickerInput = () => {
           readOnly
         />
         {isOpen && (
-          <div
-            className="absolute z-10 mt-1 h-[388px] w-[327px] rounded-lg bg-white lg:h-[584px] lg:w-[640px]"
-            onMouseDown={(e) => {
-              e.stopPropagation();
-              e.preventDefault();
-            }}
-          >
-            <DatePicker
-              inline
-              selectsRange
-              locale={ko}
-              startDate={startDate}
-              endDate={endDate}
-              onChange={handleChange}
-              renderCustomHeader={(props) => <DatePickerHeader {...props} handleOpenDropdown={handleOpenDropdown} />}
-            />
-          </div>
+          <>
+            <div
+              className="absolute z-10 mt-1 h-[388px] w-[327px] rounded-lg bg-white lg:h-[584px] lg:w-[640px]"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <DatePicker
+                inline
+                selectsRange
+                locale={ko}
+                startDate={startDate}
+                endDate={endDate}
+                onChange={handleChange}
+                renderCustomHeader={(props) => <DatePickerHeader {...props} />}
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => handleOpenDropdown()}
+              onMouseDown={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+              className="absolute left-[14px] top-[78px] z-30 cursor-pointer lg:top-[84px]"
+            >
+              <IoIosClose className="size-6 text-black-100 lg:size-9" />
+            </button>
+          </>
         )}
       </div>
     </div>
