81fabb7934e4125ad7b453a346e477ba5310d885
yyezzzy <t47047877@gmail.com>
2024-11-28 18:03:19 +0900

    chore: 간단한 수정

--- src/app/stories/design-system/components/dropdown/FilterDropdown.stories.tsx
+++ src/app/stories/design-system/components/dropdown/FilterDropdown.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from "@storybook/react";
-import Dropdown from "@/app/components/dropdown/FilterDropdown";
+import FilterDropdown from "@/app/components/dropdown/FilterDropdown";
 
-const meta: Meta<typeof Dropdown> = {
+const meta: Meta<typeof FilterDropdown> = {
   title: "Design System/Components/Dropdown/FilterDropdown",
-  component: Dropdown,
+  component: FilterDropdown,
   parameters: {
     layout: "centered",
   },
@@ -14,9 +14,9 @@ const meta: Meta<typeof Dropdown> = {
 };
 
 export default meta;
-type Story = StoryObj<typeof Dropdown>;
+type Story = StoryObj<typeof FilterDropdown>;
 
-export const FilterDropdown: Story = {
+export const Default: Story = {
   args: {
     label: "Options",
     options: ["전체", "공개", "비공개", "거절", "면접 대기", "면접 완료", "채용 완료"],
