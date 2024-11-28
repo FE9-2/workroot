3b2960fd51d7d8d4323d0c9347aa581bae2380b5
yyezzzy <t47047877@gmail.com>
2024-11-28 15:53:48 +0900

    design: card-list/모집조건_info UI 구현 완료

--- src/app/components/card/cardList/RecruitDetail.tsx
+++ src/app/components/card/cardList/RecruitDetail.tsx
@@ -5,10 +5,10 @@ import { cn } from "@/lib/tailwindUtil";
 const Devider = () => <div className="border-b-[1px] border-line-200" />;
 const RecruitDetail: React.FC<{ recruitData: FormDetailResponse }> = ({ recruitData }) => {
   const titleStyle = "text-black-100";
-  const flexStyle = "flex items-center justify-between md:py-4";
+  const propsStyle = "flex items-center justify-between md:py-4";
   return (
-    <div className="flex h-[156px] w-[375px] flex-col justify-center gap-3 rounded-lg border border-line-100 bg-white px-6 py-3 text-sm shadow-md md:h-[336px] md:w-[640px] md:gap-6 md:text-lg">
-      <div className={cn(flexStyle)}>
+    <div className="flex h-[156px] w-[375px] flex-col justify-center gap-3 rounded-lg border border-line-100 bg-white px-6 py-3 text-sm md:h-[336px] md:w-[640px] md:gap-6 md:text-lg">
+      <div className={cn(propsStyle)}>
         <div>
           <span className={cn(titleStyle)}>모집 기간</span>
           <strong className="orange-black-100 ml-4 text-primary-orange-300">D-10</strong>
@@ -20,13 +20,13 @@ const RecruitDetail: React.FC<{ recruitData: FormDetailResponse }> = ({ recruitD
       </div>
       <Devider />
       {/* 가게 전화번호 */}
-      <div className={cn(flexStyle)}>
+      <div className={cn(propsStyle)}>
         <span className={cn(titleStyle)}>가게 전화번호</span>
         <strong>{recruitData.storePhoneNumber}</strong>
       </div>
       <Devider />
       {/* 사장님 전화번호 */}
-      <div className={cn(flexStyle)}>
+      <div className={cn(propsStyle)}>
         <span className={cn(titleStyle)}>사장님 전화번호</span>
         <strong>{recruitData.phoneNumber}</strong>
       </div>
--- src/app/stories/design-system/components/card/cardList/RecruitCond.stories.tsx
+++ src/app/stories/design-system/components/card/cardList/RecruitCond.stories.tsx
@@ -1,26 +1,26 @@
 import { Meta, StoryObj } from "@storybook/react";
-import RecruitDetail from "@/app/components/card/cardList/RecruitDetail";
+import RecruitCond from "@/app/components/card/cardList/RecruitCond";
 import { FormDetailResponse } from "@/types/response/form";
 
-const meta: Meta<typeof RecruitDetail> = {
-  title: "Design System/Components/Card/CardList/RecruitDetail",
-  component: RecruitDetail,
+const meta: Meta<typeof RecruitCond> = {
+  title: "Design System/Components/Card/CardList/RecruitCond",
+  component: RecruitCond,
   parameters: {
     layout: "centered",
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof RecruitDetail>;
+type Story = StoryObj<typeof RecruitCond>;
 
 const mockRecruitData: FormDetailResponse = {
   updatedAt: new Date(),
   createdAt: new Date(),
-  preferred: "string",
-  age: "string",
-  education: "string",
-  gender: "string",
-  numberOfPositions: 1,
+  preferred: "업무 관련 자격증 소지, 유사업무 경험 우대, 인근 거주 우대",
+  age: "20세 이상",
+  education: "고등학교 졸업",
+  gender: "무관",
+  numberOfPositions: 3,
   isPublic: true,
   hourlyWage: 10000,
   isNegotiableWorkDays: true,
@@ -33,8 +33,8 @@ const mockRecruitData: FormDetailResponse = {
   imageUrls: ["url1", "url2"],
   recruitmentEndDate: new Date(),
   recruitmentStartDate: new Date(),
-  description: "모집 설명",
-  title: "모집 제목",
+  description: "모집설명",
+  title: "모집제목",
   ownerId: 1,
   id: 1,
   scrapCount: 0,
@@ -42,7 +42,7 @@ const mockRecruitData: FormDetailResponse = {
   isScrapped: false,
   phoneNumber: "010-1234-5678",
   storePhoneNumber: "02-123-4567",
-  storeName: "가게 이름",
+  storeName: "가게이름",
 };
 
 export const Default: Story = {
