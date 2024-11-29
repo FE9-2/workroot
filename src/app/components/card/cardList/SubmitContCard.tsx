71996a97046a4d15a9a3a3d36cdda5d69b8af119
yyezzzy <t47047877@gmail.com>
2024-11-29 14:58:43 +0900

    design: 지원내용 카드 컴포넌트 SubmitContCard.tsx UI 구현 완료

--- src/app/components/card/cardList/SubmitContCard.tsx
+++ src/app/components/card/cardList/SubmitContCard.tsx
@@ -1,25 +1,35 @@
 "use client";
 import { ApplicationResponse } from "@/types/response/application";
 import React from "react";
+import { cn } from "@/lib/tailwindUtil";
 
 interface SubmitContCardProps {
   submitContData: ApplicationResponse;
 }
 
 // 지원내용 카드 컴포넌트
+const Devider = () => <div className="border-b-[1px] border-line-200" />;
+
 const SubmitContCard = ({ submitContData }: SubmitContCardProps) => {
+  const titleStyle = "text-black-100";
+  const propsStyle = "flex items-center justify-between py-4";
+
   return (
-    <div className="rounded border p-4 shadow">
-      <h2 className="text-lg font-bold">지원자 정보</h2>
-      <p>
-        <strong>이름:</strong> {submitContData.name}
-      </p>
-      <p>
-        <strong>연락처:</strong> {submitContData.phoneNumber}
-      </p>
-      <p>
-        <strong>경력:</strong> {submitContData.experienceMonths} 개월
-      </p>
+    <div className="flex w-[770px] flex-col justify-center gap-4 bg-white px-6 text-base">
+      <div className={cn(propsStyle)}>
+        <span className={cn(titleStyle)}>이름</span>
+        <strong>{submitContData.name}</strong>
+      </div>
+      <Devider />
+      <div className={cn(propsStyle)}>
+        <span className={cn(titleStyle)}>연락처</span>
+        <strong>{submitContData.phoneNumber}</strong>
+      </div>
+      <Devider />
+      <div className={cn(propsStyle)}>
+        <span className={cn(titleStyle)}>경력</span>
+        <strong>{submitContData.experienceMonths} 개월</strong>
+      </div>
     </div>
   );
 };
