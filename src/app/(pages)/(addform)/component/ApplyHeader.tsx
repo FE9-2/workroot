5e8245e1749a9e7f7c04f8df537ef59ac677abb6
hongggy <hongggy@gmail.com>
2024-11-30 20:21:46 +0900

    chore: apply 헤더 스타일 수정

--- src/app/(pages)/(addform)/component/ApplyHeader.tsx
+++ src/app/(pages)/(addform)/component/ApplyHeader.tsx
@@ -3,8 +3,8 @@ import Button from "@/app/components/button/default/Button";
 const ApplyHeader = ({ title }: { title: string }) => {
   return (
     <div className="flex w-full items-center justify-between py-5">
-      <h1 className="text-xl font-semibold leading-5 leading-[46px] text-black-500 lg:text-[32px]">{title}</h1>
-      <Button className="text-grayscale-50 h-10" color="gray" width="xs">
+      <h1 className="text-xl font-semibold leading-5 text-black-500 lg:text-[32px] lg:leading-[46px]">{title}</h1>
+      <Button className="h-10 text-grayscale-50" color="gray" width="xs">
         작성 취소
       </Button>
     </div>
