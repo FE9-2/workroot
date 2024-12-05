c0c85d94cf97e910942d8392a3b5c046bfceeccd
hongggy <hongggy@gmail.com>
2024-12-05 15:18:38 +0900

    fix: location input 수정

--- src/app/components/input/text/LocationInput.tsx
+++ src/app/components/input/text/LocationInput.tsx
@@ -4,10 +4,11 @@ import { IoLocationSharp } from "react-icons/io5";
 import BaseInput from "./BaseInput";
 import { BaseInputProps } from "@/types/textInput";
 
-const LocationInput = ({ type = "text", errormessage, feedbackMessage, ...props }: BaseInputProps) => {
+const LocationInput = ({ type = "text", variant, errormessage, feedbackMessage, ...props }: BaseInputProps) => {
   return (
     <BaseInput
       type={type}
+      variant={variant || "white"}
       beforeIcon={<IoLocationSharp className="size-6 text-grayscale-100 lg:size-8" />}
       placeholder="위치를 입력해주세요."
       errormessage={errormessage}
