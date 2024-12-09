397b08a809369676182f97eb722f03a8efd41541
yyezzzy <t47047877@gmail.com>
2024-12-09 17:10:57 +0900

    feat: 알바폼 상세 페이지 생성

--- src/app/(pages)/albaFormDetail/page.tsx
+++ src/app/(pages)/albaFormDetail/page.tsx
@@ -2,5 +2,9 @@ import React from "react";
 
 // 알바폼 상세
 export default function AlbaFormDetail() {
-  return <div>AlbaFormDetail</div>;
+  return (
+    <div className="flex min-h-screen min-w-[1560px] flex-col items-center">
+      <div className="w-inherit flex bg-black-100">지도영역</div>
+    </div>
+  );
 }
