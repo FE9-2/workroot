8bd770ae6cb1ebb13e32ddce8c0601864b779f3a
hongggy <hongggy@gmail.com>
2024-12-09 20:27:40 +0900

    fix: Suspense적용

--- src/app/(pages)/(albaform)/layout.tsx
+++ src/app/(pages)/(albaform)/layout.tsx
@@ -1,10 +1,14 @@
 "use client";
 import AddFormLayout from "@/app/components/layout/addFormLayout/AddFormLayout";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const title = pathname.split("/").includes("apply") ? "알바폼 지원하기" : "알바폼 만들기";
-  return <AddFormLayout title={title}>{children}</AddFormLayout>;
+  return (
+    <AddFormLayout title={title}>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </AddFormLayout>
+  );
 }
