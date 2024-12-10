ac7f2832487317d96938ccb76c05a1f0f0eee0ff
yyezzzy <t47047877@gmail.com>
2024-12-10 15:58:38 +0900

    feat: useFormDetail 추가

---
+++ src/hooks/queries/form/userFormDetail.ts
@@ -0,0 +1,27 @@
+import { FormDetailResponse } from "@/types/response/form";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface UseUserFormDetailParams {
+  formId?: number;
+}
+
+export const useUserFormDetail = ({ formId }: UseUserFormDetailParams) => {
+  const query = useQuery<FormDetailResponse>({
+    queryKey: ["formDetail", formId],
+    queryFn: async () => {
+      if (!formId) {
+        throw new Error("formId가 없습니다.");
+      }
+      const response = await axios.get(`/api/forms/${formId}`);
+      return response.data;
+    },
+    enabled: !!formId, // formId가 유효한 경우에만 쿼리를 실행
+  });
+
+  return {
+    ...query,
+    isPending: query.isPending,
+    error: query.error,
+  };
+};
