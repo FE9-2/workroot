7fbed89b7c4a520d90ae2a45bb189de7066b769e
hongggy <hongggy@gmail.com>
2024-12-11 10:40:57 +0900

    refactor: type 분리

--- src/app/(pages)/(albaform)/addform/page.tsx
+++ src/app/(pages)/(albaform)/addform/page.tsx
@@ -12,29 +12,8 @@ import RecruitContentSection from "./RecruitContentSection";
 import RecruitConditionSection from "./RecruitConditionSection";
 import WorkConditionSection from "./WorkConditionSection";
 import useEditing from "@/hooks/useEditing";
+import { SubmitFormDataType } from "@/types/addform";
 
-interface SubmitFormDataType {
-  isPublic: boolean;
-  hourlyWage: number;
-  isNegotiableWorkDays: boolean;
-  workDays: string[];
-  workEndTime: string;
-  workStartTime: string;
-  workEndDate: string;
-  workStartDate: string;
-  location: string;
-  preferred: string;
-  age: string;
-  education: string;
-  gender: string;
-  numberOfPositions: number;
-  imageUrls: string[];
-  recruitmentEndDate: string | undefined;
-  recruitmentStartDate: string | undefined;
-  description: string;
-  title: string;
-  imageFiles: File[];
-}
 export default function AddFormPage() {
   const router = useRouter();
 
