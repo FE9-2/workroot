e3c1f905bb0fb0e9130d041c202501cdb70679f2
yyezzzy <t47047877@gmail.com>
2024-12-12 01:04:33 +0900

    chore: thead, tbody 간격 조정

--- src/app/components/card/cardList/ApplicationStatusCard.tsx
+++ src/app/components/card/cardList/ApplicationStatusCard.tsx
@@ -12,14 +12,14 @@ const ApplicationStatusCard = ({ applicationStatusData }: ApplicationStatusCardP
   const [experienceSort, setExperienceSort] = useState(false);
   const [statusSort, setStatusSort] = useState(false);
 
-  const theadStyle = "text-left font-semibold text-grayscale-700";
-  const tbodyStyle = "text-left";
+  const theadStyle = "text-center font-semibold text-grayscale-700";
+  const tbodyStyle = "text-center";
   const sortIconStyle = "text-primary-orange-300 transition-transform hover:scale-110";
   return (
     <div className="w-[375px] text-xs md:w-[770px] md:text-base">
       <div className="flex flex-col">
         {/* Thead */}
-        <div className="sticky grid grid-cols-[1fr_2fr_0.6fr_0.6fr] px-6 py-4">
+        <div className="sticky grid grid-cols-[1fr_2fr_2fr_1fr] border-b border-line-100 px-6 py-4">
           <span className={theadStyle}>이름</span>
           <span className={theadStyle}>전화번호</span>
           <div className="flex items-center gap-2">
@@ -29,7 +29,7 @@ const ApplicationStatusCard = ({ applicationStatusData }: ApplicationStatusCardP
             </button>
           </div>
           <div className="flex items-center gap-2">
-            <span className="text-grayscale-700 text-left font-semibold">상태</span>
+            <span className={theadStyle}>상태</span>
             <button onClick={() => setStatusSort((prev) => !prev)}>
               <FaSortAmountDown className={cn(sortIconStyle, statusSort ? "rotate-0" : "rotate-180")} />
             </button>
@@ -39,10 +39,7 @@ const ApplicationStatusCard = ({ applicationStatusData }: ApplicationStatusCardP
         {/* Tbody */}
         <div className="scrollbar-custom h-[360px] overflow-y-auto md:h-[400px]">
           {applicationStatusData.map((application) => (
-            <div
-              key={application.id}
-              className="grid grid-cols-[1fr_2fr_0.6fr_0.6fr] border-b border-line-100 px-6 py-4"
-            >
+            <div key={application.id} className="grid grid-cols-[1fr_2fr_2fr_1fr] border-b border-line-100 px-6 py-4">
               <span className={tbodyStyle}>{application.name}</span>
               <span className={tbodyStyle}>{application.phoneNumber}</span>
               <span className={tbodyStyle}>{application.experienceMonths}개월</span>
