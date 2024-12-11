dd287ca2451c60bf88743b7c2c44c66d5a67eafc
yyezzzy <t47047877@gmail.com>
2024-12-11 19:30:31 +0900

    chore: ApplicationStatusCard 수정

--- src/app/components/card/cardList/ApplicationStatusCard.tsx
+++ src/app/components/card/cardList/ApplicationStatusCard.tsx
@@ -1,18 +1,61 @@
 "use client";
-
-import React from "react";
+import React, { useState } from "react";
 import { ApplicationResponse } from "@/types/response/application";
-
+import { FaSortAmountDown } from "react-icons/fa";
+import { cn } from "@/lib/tailwindUtil";
 interface ApplicationStatusCardProps {
-  applicationStatusData: ApplicationResponse[]; // 전달받는 데이터는 배열
+  applicationStatusData: ApplicationResponse[];
 }
 
-export default function ApplicationStatusCard({ applicationStatusData }: ApplicationStatusCardProps) {
+// 지원현황 카드 컴포넌트
+const ApplicationStatusCard = ({ applicationStatusData }: ApplicationStatusCardProps) => {
+  const [sortOrder, setSortOrder] = useState(false);
+
+  const handleSort = () => {
+    setSortOrder((prev) => !prev);
+  };
+
+  const theadStyle = "text-left font-semibold text-grayscale-700";
+  const tbodyStyle = "text-left";
+  const sortIconStyle = "text-primary-orange-300 transition-transform hover:scale-110";
   return (
-    <div>
-      {applicationStatusData.map((application) => (
-        <div key={application.id}>{application.name}</div> // 고유 키 추가
-      ))}
+    <div className="w-[375px] text-xs md:w-[770px] md:text-base">
+      <div className="flex flex-col">
+        {/* Thead */}
+        <div className="sticky grid grid-cols-[1fr_2fr_0.6fr_0.6fr] px-6 py-4">
+          <span className={theadStyle}>이름</span>
+          <span className={theadStyle}>전화번호</span>
+          <div className="flex items-center gap-2">
+            <span className={theadStyle}>경력</span>
+            <button>
+              <FaSortAmountDown className={cn(sortIconStyle, sortOrder ? "rotate-0" : "rotate-180")} />
+            </button>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-grayscale-700 text-left font-semibold">상태</span>
+            <button onClick={() => handleSort()}>
+              <FaSortAmountDown className={cn(sortIconStyle, sortOrder ? "rotate-0" : "rotate-180")} />
+            </button>
+          </div>
+        </div>
+
+        {/* Tbody */}
+        <div className="scrollbar-custom h-[360px] overflow-y-auto md:h-[400px]">
+          {applicationStatusData.map((application) => (
+            <div
+              key={application.id}
+              className="grid grid-cols-[1fr_2fr_0.6fr_0.6fr] border-b border-line-100 px-6 py-4"
+            >
+              <span className={tbodyStyle}>{application.name}</span>
+              <span className={tbodyStyle}>{application.phoneNumber}</span>
+              <span className={tbodyStyle}>{application.experienceMonths}개월</span>
+              <span className={tbodyStyle}>{application.status}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default ApplicationStatusCard;
