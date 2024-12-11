a3c1e67fece4bb8ddbdbdab72a619549c15d43df
hongggy <hongggy@gmail.com>
2024-12-11 16:49:54 +0900

    chore: 미사용 import 수정

--- src/app/components/card/cardList/ApplicationStatusCard.tsx
+++ src/app/components/card/cardList/ApplicationStatusCard.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { ApplicationResponse } from "@/types/response/application";
-import { FaSortAmountDown } from "react-icons/fa";
-import { cn } from "@/lib/tailwindUtil";
 
 interface ApplicationStatusCardProps {
   applicationStatusData: ApplicationResponse[]; // 전달받는 데이터는 배열
