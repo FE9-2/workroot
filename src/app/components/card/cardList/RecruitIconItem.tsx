a09b0ac02f365a585972dc67fd7e583b5ef8e43f
hongggy <hongggy@gmail.com>
2024-12-12 16:11:50 +0900

    fix: 높이 수정

--- src/app/components/card/cardList/RecruitIconItem.tsx
+++ src/app/components/card/cardList/RecruitIconItem.tsx
@@ -12,7 +12,7 @@ interface RecruitIconItemProps {
 // 채용 공고의 근무 조건을 아이콘으로 표시하는 컴포넌트의 아이템 컴포넌트
 const RecruitIconItem = ({ icon, label, value }: RecruitIconItemProps) => {
   return (
-    <div className="flex h-[54px] items-center gap-2 overflow-hidden rounded-lg border border-grayscale-200 p-2 lg:h-[167px] lg:gap-6 lg:p-[30px]">
+    <div className="flex h-[54px] items-start gap-2 overflow-hidden rounded-lg border border-grayscale-200 p-2 lg:h-[150px] lg:gap-6 lg:p-[30px]">
       <div className="flex h-9 w-9 items-center justify-center rounded-full bg-grayscale-100 bg-opacity-30 lg:h-14 lg:w-14">
         <div className="block lg:hidden">
           <Image src={icon.sm} alt={label} width={24} height={24} />
