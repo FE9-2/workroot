a67330f8c7e7eb23fd6d487277990e951529e879
hongggy <hongggy@gmail.com>
2024-12-12 15:54:49 +0900

    refactor: RecruitIcon css수정

--- src/app/components/card/cardList/RecruitIconItem.tsx
+++ src/app/components/card/cardList/RecruitIconItem.tsx
@@ -12,21 +12,20 @@ interface RecruitIconItemProps {
 // 채용 공고의 근무 조건을 아이콘으로 표시하는 컴포넌트의 아이템 컴포넌트
 const RecruitIconItem = ({ icon, label, value }: RecruitIconItemProps) => {
   return (
-    <div className="flex items-center gap-2 overflow-hidden rounded-lg border border-grayscale-200 p-1 sm:p-3 md:gap-6 md:p-6">
-      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-grayscale-100 bg-opacity-30 sm:h-9 sm:w-9 md:h-16 md:w-16">
-        <div className="block md:hidden">
+    <div className="flex h-[54px] items-center gap-2 overflow-hidden rounded-lg border border-grayscale-200 p-2 lg:h-[167px] lg:gap-6 lg:p-[30px]">
+      <div className="flex h-9 w-9 items-center justify-center rounded-full bg-grayscale-100 bg-opacity-30 lg:h-14 lg:w-14">
+        <div className="block lg:hidden">
           <Image src={icon.sm} alt={label} width={24} height={24} />
         </div>
-        <div className="hidden md:block">
+        <div className="hidden lg:block">
           <Image src={icon.md} alt={label} width={36} height={36} />
         </div>
       </div>
       <div className="flex-1">
-        <div className="text-grayscale-600 text-[10px] font-medium sm:text-xs md:text-xl">{label}</div>
-        <div className="text-xs font-semibold text-primary-orange-300 sm:text-sm md:text-2xl">{value}</div>
+        <div className="text-grayscale-600 text-[10px] font-medium sm:text-xs lg:text-xl">{label}</div>
+        <div className="text-xs font-semibold text-primary-orange-300 sm:text-sm lg:text-2xl">{value}</div>
       </div>
     </div>
   );
 };
-
 export default RecruitIconItem;
