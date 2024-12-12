8b37c6a04584f79cbec9f3869d8e8344adf3263e
hongggy <hongggy@gmail.com>
2024-12-12 15:06:48 +0900

    refactor: alba/id 폴더 병합 및 레이아웃 수정

--- src/app/(pages)/(albaform)/alba/[formId]/page.tsx
+++ src/app/(pages)/(albaform)/alba/[formId]/page.tsx
@@ -3,10 +3,10 @@
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { useUser } from "@/hooks/queries/user/me/useUser";
-import FormHeader from "../components/FormHeader";
-import FormDetails from "../components/FormDetail";
-import RecruitInformation from "../components/RecruitInfomation";
-import ApplicationStatus from "../components/ApplicationStatus";
+import FormHeader from "./edit/components/FormHeader";
+import FormDetails from "./edit/components/FormDetail";
+import RecruitInformation from "./edit/components/RecruitInfomation";
+import ApplicationStatus from "./edit/components/ApplicationStatus";
 import { useFormDetail } from "@/hooks/queries/form/detail/useFormDetail";
 
 export default function AlbaFormDetailPage() {
