99ea922ce6c6dd430ef330953d76c397a06da5ef
yyezzzy <t47047877@gmail.com>
2024-12-14 02:48:20 +0900

    refactor: MyApplicationListItem에 getStatusVariant 함수 적용

--- src/app/components/card/cardList/MyApplicationListItem.tsx
+++ src/app/components/card/cardList/MyApplicationListItem.tsx
@@ -2,24 +2,12 @@ import { getRecruitmentStatus } from "@/utils/recruitDateFormatter";
 import { formatLocalDate } from "@/utils/workDayFormatter";
 import Chip from "@/app/components/chip/Chip";
 import Image from "next/image";
-import { applicationStatus, ApplyStatus } from "@/types/application";
+import { ApplyStatus } from "@/types/application";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { MyApplicationType } from "@/types/response/user";
 import { MdOutlineImage } from "react-icons/md";
-import translateStatus from "@/utils/translateStatus";
-
-// 지원 상태에 따른 Chip 컴포넌트의 variant를 반환하는 함수
-const getStatusVariant = (status: ApplyStatus) => {
-  switch (status) {
-    case applicationStatus.HIRED:
-      return "positive";
-    case applicationStatus.REJECTED:
-      return "negative";
-    default:
-      return "positive";
-  }
-};
+import translateStatus, { getStatusVariant } from "@/utils/translateStatus";
 
 const MyApplicationListItem = ({ createdAt, status, resumeId, resumeName, form }: MyApplicationType) => {
   // 이력서 다운로드 핸들러
--- src/utils/translateStatus.ts
+++ src/utils/translateStatus.ts
@@ -1,12 +1,34 @@
-const translateStatus = (status: string) => {
-  const statusMap: { [key: string]: string } = {
-    ALL: "전체",
-    HIRED: "채용 완료",
-    REJECTED: "거절",
-    INTERVIEW_PENDING: "면접 대기",
-    INTERVIEW_COMPLETED: "면접 완료",
-  };
+import { applicationStatus, ApplyStatus } from "@/types/application";
+
+// 지원 상태에 따른 Chip 컴포넌트의 variant를 반환하는 함수
+export const getStatusVariant = (status: ApplyStatus) => {
+  switch (status) {
+    case applicationStatus.HIRED:
+      return "positive";
+    case applicationStatus.REJECTED:
+      return "negative";
+    default:
+      return "positive";
+  }
+};
+
+// 상태를 한글로 변환하는 함수
+const statusMap: { [key: string]: string } = {
+  ALL: "전체",
+  HIRED: "채용 완료",
+  REJECTED: "거절",
+  INTERVIEW_PENDING: "면접 대기",
+  INTERVIEW_COMPLETED: "면접 완료",
+};
+
+// 상태를 변환하는 함수
+export const getStatusMap = (status: ApplyStatus) => {
   return statusMap[status] || status;
+};
+
+// 기본 내보내기
+const translateStatus = (status: ApplyStatus) => {
+  return getStatusMap(status);
 };
 
 export default translateStatus;
