09571a7b13137ee414dbb5c797e7e323fbdac66c
hongggy <hongggy@gmail.com>
2024-12-14 15:39:36 +0900

    refactor: 알바톡 edit 페이지 수정

--- src/app/(pages)/albaTalk/[albatalkId]/edit/page.tsx
+++ src/app/(pages)/albaTalk/[albatalkId]/edit/page.tsx
@@ -144,117 +144,108 @@ export default function EditTalk({ params }: { params: { albatalkId: string } })
   if (!post) return null;
 
   return (
-    <>
-      <div className="flex min-h-screen w-full items-start justify-center bg-grayscale-50 font-nexon">
-        <div className="w-full max-w-[1480px] rounded-md bg-white px-4 md:px-8 lg:px-10">
-          <div className="mb-[40px] flex h-[58px] w-full items-center justify-between border-b border-[#line-100] md:h-[78px] lg:h-[126px]">
-            <div className="flex items-center text-[18px] font-semibold md:text-[20px] lg:text-[32px]">
-              게시글 수정하기
-            </div>
-            <div className="hidden space-x-1 font-semibold md:flex md:space-x-2 lg:space-x-4">
-              <Button
-                variant="solid"
-                color="gray"
-                className="md:h-[46px] md:w-[108px] md:text-[14px] lg:h-[58px] lg:w-[180px] lg:text-[18px]"
-                onClick={() => router.push(`/albatalk/${postId}`)}
-              >
-                취소
-              </Button>
-              <Button
-                variant="solid"
-                color="orange"
-                className="md:h-[46px] md:w-[108px] md:text-[14px] lg:h-[58px] lg:w-[180px] lg:text-[18px]"
-                onClick={handleSubmit(onSubmit)}
-                disabled={isPending}
-              >
-                {isPending ? <DotLoadingSpinner /> : "수정하기"}
-              </Button>
-            </div>
+    <div className="flex w-full flex-col bg-grayscale-50 px-6 font-nexon lg:px-10">
+      <div className="w-full max-w-[1480px] rounded-md bg-white">
+        <div className="mb-[40px] flex h-[58px] w-full items-center justify-between border-b border-line-100 md:h-[78px] lg:h-[126px]">
+          <div className="flex items-center text-[18px] font-semibold md:text-[20px] lg:text-[32px]">
+            게시글 수정하기
           </div>
-          <form className="mx-auto max-w-[1432px] space-y-6 px-2 md:space-y-8" onSubmit={handleSubmit(onSubmit)}>
-            <div className="w-full">
-              <label
-                htmlFor="title"
-                className="mb-2 flex items-center text-[16px] font-medium text-black-300 md:text-[18px] lg:text-[20px]"
-              >
-                제목<span className="ml-1 text-primary-orange-300">*</span>
-              </label>
-              <Controller
-                name="title"
-                control={control}
-                rules={{ required: "제목을 입력하세요." }}
-                render={({ field }) => (
-                  <BaseInput
-                    {...field}
-                    type="text"
-                    variant="white"
-                    name="title"
-                    size="w-full h-[52px] md:h-[54px] lg:h-[64px]"
-                    placeholder="제목을 입력하세요"
-                    errormessage={errors.title?.message}
-                  />
-                )}
-              />
-            </div>
-
-            <div className="w-full">
-              <label
-                htmlFor="content"
-                className="mb-2 flex items-center text-[16px] font-medium text-black-300 md:text-[18px] lg:text-[20px]"
-              >
-                내용<span className="ml-1 text-primary-orange-300">*</span>
-              </label>
-              <Controller
-                name="content"
-                control={control}
-                rules={{ required: "내용을 입력하세요." }}
-                render={({ field }) => (
-                  <textarea
-                    {...field}
-                    className="h-[240px] w-full resize-none rounded-lg bg-background-200 p-[14px] text-[16px] placeholder:text-grayscale-400 focus:outline-none focus:ring-2 focus:ring-primary-orange-300 md:text-[18px] lg:text-[20px]"
-                    placeholder="내용을 입력하세요"
-                  />
-                )}
-              />
-            </div>
-
-            <div className="w-full">
-              <label
-                htmlFor="image"
-                className="mb-2 block text-[16px] font-medium text-black-300 md:text-[18px] lg:text-[20px]"
-              >
-                이미지
-              </label>
-              <ImageInputPlaceHolder
-                onImageUpload={uploadImage}
-                onImagesChange={handleImagesChange}
-                initialImages={imageList}
-              />
-            </div>
-          </form>
+          <div className="hidden space-x-1 font-semibold md:flex md:space-x-2 lg:space-x-4">
+            <Button
+              color="gray"
+              className="text-grayscale-50md:h-[46px] text-white md:w-[108px] md:text-[14px] lg:h-[58px] lg:w-[180px] lg:text-[18px]"
+              onClick={() => router.push("/albaTalk")}
+            >
+              취소
+            </Button>
+            <Button
+              className="text-grayscale-50 md:h-[46px] md:w-[108px] md:text-[14px] lg:h-[58px] lg:w-[180px] lg:text-[18px]"
+              onClick={handleSubmit(onSubmit)}
+              disabled={isPending}
+            >
+              {isPending ? <DotLoadingSpinner /> : "수정하기"}
+            </Button>
+          </div>
         </div>
-      </div>
+        <form className="mx-auto max-w-[1432px] space-y-6 md:space-y-8" onSubmit={handleSubmit(onSubmit)}>
+          <div className="w-full">
+            <label
+              htmlFor="title"
+              className="mb-2 flex items-center text-[16px] font-medium text-black-300 md:text-[18px] lg:text-[20px]"
+            >
+              제목<span className="ml-1 text-primary-orange-300">*</span>
+            </label>
+            <Controller
+              name="title"
+              control={control}
+              rules={{ required: "제목을 입력하세요." }}
+              render={({ field }) => (
+                <BaseInput
+                  {...field}
+                  type="text"
+                  variant="white"
+                  name="title"
+                  size="w-full h-[52px] md:h-[54px] lg:h-[64px]"
+                  placeholder="제목을 입력하세요"
+                  errormessage={errors.title?.message}
+                />
+              )}
+            />
+          </div>
 
+          <div className="w-full">
+            <label
+              htmlFor="content"
+              className="mb-2 flex items-center text-[16px] font-medium text-black-300 md:text-[18px] lg:text-[20px]"
+            >
+              내용<span className="ml-1 text-primary-orange-300">*</span>
+            </label>
+            <Controller
+              name="content"
+              control={control}
+              rules={{ required: "내용을 입력하세요." }}
+              render={({ field }) => (
+                <textarea
+                  {...field}
+                  className="h-[240px] w-full resize-none rounded-lg bg-background-200 p-[14px] text-[16px] placeholder:text-grayscale-400 focus:outline-none focus:ring-2 focus:ring-primary-orange-300 md:text-[18px] lg:text-[20px]"
+                  placeholder="내용을 입력하세요"
+                />
+              )}
+            />
+          </div>
+
+          <div className="w-full">
+            <label
+              htmlFor="image"
+              className="mb-2 block text-[16px] font-medium text-black-300 md:text-[18px] lg:text-[20px]"
+            >
+              이미지
+            </label>
+            <ImageInputPlaceHolder
+              onImageUpload={uploadImage}
+              onImagesChange={handleImagesChange}
+              initialImages={imageList}
+            />
+          </div>
+        </form>
+      </div>
       {/* 모바일 버전 버튼 */}
-      <div className="fixed bottom-4 left-4 right-4 flex w-full flex-col items-center space-y-2 rounded-t-lg bg-white p-4 font-semibold md:hidden">
+      <div className="flex w-full flex-col items-center justify-center space-y-2 rounded-t-lg bg-white py-10 font-semibold md:hidden">
         <Button
-          variant="solid"
           color="gray"
-          className="h-[58px] w-[327px] rounded-[8px]"
-          onClick={() => router.push(`/albatalk/${postId}`)}
+          className="mb-2 h-[58px] w-full rounded-[8px] bg-grayscale-100 text-white hover:bg-grayscale-200"
+          onClick={() => router.push("/albaTalk")}
         >
           취소
         </Button>
         <Button
-          variant="solid"
-          color="orange"
-          className="h-[58px] w-[327px] rounded-[8px]"
+          className="h-[58px] w-full rounded-[8px] bg-primary-orange-300 text-white hover:bg-orange-400"
           onClick={handleSubmit(onSubmit)}
           disabled={isPending}
         >
           {isPending ? <DotLoadingSpinner /> : "수정하기"}
         </Button>
       </div>
-    </>
+    </div>
   );
 }
