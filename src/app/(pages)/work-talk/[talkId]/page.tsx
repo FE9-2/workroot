b5eef414d7514aebf8de5406d800b2deae21573f
cccwon2 <cccwon2@naver.com>
2024-12-17 12:04:44 +0900

    fix: 워크톡 > 상세 id 관련 버그 수정

--- src/app/(pages)/work-talk/[talkId]/page.tsx
+++ src/app/(pages)/work-talk/[talkId]/page.tsx
@@ -5,12 +5,12 @@ import { PostDetailSection } from "./sections/PostDetailSection";
 import { CommentsSection } from "./sections/CommentsSection";
 
 export default function PostDetailPage() {
-  const { albatalkId } = useParams();
+  const { talkId } = useParams();
 
   return (
     <div className="mx-auto flex w-full min-w-[375px] max-w-screen-xl flex-col items-center px-4">
-      <PostDetailSection postId={albatalkId.toString()} />
-      <CommentsSection postId={albatalkId.toString()} />
+      <PostDetailSection postId={talkId.toString()} />
+      <CommentsSection postId={talkId.toString()} />
     </div>
   );
 }
