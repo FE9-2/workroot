767b73f0e20b472fabfeb094844df225b40a1597
yyezzzy <t47047877@gmail.com>
2024-12-22 19:19:32 +0900

    feat: 이미지 드래그 앤 드롭 기능 추가

--- src/app/components/input/file/ImageInput/ImageInput.tsx
+++ src/app/components/input/file/ImageInput/ImageInput.tsx
@@ -2,13 +2,14 @@
 import { HiUpload } from "react-icons/hi";
 import { forwardRef, useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
 import PreviewItem from "./PreviewItem";
 import { cn } from "@/lib/tailwindUtil";
 import { ImageInputType } from "@/types/addform";
 
 interface ImageInputProps {
   name: string;
-  onChange?: (files: File[]) => void;
+  onChange?: (files: File[] | string[]) => void;
   onDelete?: (id: string) => void;
   initialImageList: ImageInputType[];
 }
@@ -66,34 +67,61 @@ const ImageInput = forwardRef<HTMLInputElement, ImageInputProps>((props, ref) =>
     innerHoverColor: "hover:bg-background-300",
   };
 
+  const handleDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const items = Array.from(imageList);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setImageList(items);
+    // 상위 컴포넌트에 변경된 이미지 URL 배열 전달
+    props.onChange?.(items.map((item) => item.url));
+  };
+
   return (
-    <div className="flex gap-5 lg:gap-6">
-      <div
-        onClick={handleOpenFileSelector}
-        className={cn(
-          "relative size-20 cursor-pointer rounded-lg lg:size-[116px]",
-          colorStyle.bgColor,
-          colorStyle.borderColor,
-          colorStyle.hoverColor
-        )}
-      >
-        <input
-          ref={ref}
-          type="file"
-          name={props.name}
-          accept="image/*"
-          onChange={(e) => handleFileChange(e.target.files)}
-          className="hidden"
-          multiple
-        />
-        <div className="pointer-events-none absolute top-0 z-10 p-7 lg:p-10">
-          <HiUpload className="text-[24px] text-grayscale-400 lg:text-[36px]" />
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div className="flex gap-5 lg:gap-6">
+        <div
+          onClick={handleOpenFileSelector}
+          className={cn(
+            "relative size-20 cursor-pointer rounded-lg lg:size-[116px]",
+            colorStyle.bgColor,
+            colorStyle.borderColor,
+            colorStyle.hoverColor
+          )}
+        >
+          <input
+            ref={ref}
+            type="file"
+            name={props.name}
+            accept="image/*"
+            onChange={(e) => handleFileChange(e.target.files)}
+            className="hidden"
+            multiple
+          />
+          <div className="pointer-events-none absolute top-0 z-10 p-7 lg:p-10">
+            <HiUpload className="text-[24px] text-grayscale-400 lg:text-[36px]" />
+          </div>
         </div>
+        <Droppable droppableId="image-list" direction="horizontal">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef} className="flex gap-5 lg:gap-6">
+              {imageList.map((image, index) => (
+                <Draggable key={image.id} draggableId={image.id} index={index}>
+                  {(provided) => (
+                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <PreviewItem image={image} handleDeleteImage={handleDeleteImage} placeholder={false} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       </div>
-      {imageList.map((image) => (
-        <PreviewItem key={image.id} image={image} handleDeleteImage={handleDeleteImage} placeholder={false} />
-      ))}
-    </div>
+    </DragDropContext>
   );
 });
 
