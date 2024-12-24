403757921a45882eaa54965c95bff40dfa6218b9
hongggy <hongggy@gmail.com>
2024-12-24 15:06:35 +0900

    chore: 마이페이지 비회원 접근시 토스트 추가

--- src/app/(pages)/mypage/page.tsx
+++ src/app/(pages)/mypage/page.tsx
@@ -6,10 +6,13 @@ import CommentsSection from "./components/sections/CommentsSection";
 import ScrapsSection from "./components/sections/ScrapsSection";
 import { userRoles } from "@/constants/userRoles";
 import { useUser } from "@/hooks/queries/user/me/useUser";
+import toast from "react-hot-toast";
+import { useRouter } from "next/router";
 
 export default function MyPage() {
   const searchParams = useSearchParams();
   const currentTab = searchParams.get("tab") || "posts";
+  const router = useRouter();
   const { user } = useUser();
   const isApplicant = user?.role === userRoles.APPLICANT;
 
@@ -18,6 +21,11 @@ export default function MyPage() {
     comments: <CommentsSection />,
     ...(isApplicant && { scrap: <ScrapsSection /> }),
   }[currentTab];
+
+  if (!user) {
+    toast.error("로그인이 필요한 페이지입니다.");
+    router.push("/login");
+  }
 
   return TabContent;
 }
