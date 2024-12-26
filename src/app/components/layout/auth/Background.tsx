ea5bd0ca8427b2403cebb5dc491d156769fc1a2b
yyezzzy <t47047877@gmail.com>
2024-12-26 14:51:10 +0900

    chore: 로그인, 회원가입 페이지 배경색 수정

--- src/app/components/layout/auth/Background.tsx
+++ src/app/components/layout/auth/Background.tsx
@@ -1,6 +1,6 @@
 export default function Background({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mt-[64px] flex min-h-[calc(100vh-64px)] items-center justify-center bg-gradient-to-r from-primary-orange-100 to-primary-orange-200 px-4 py-12 sm:px-6 lg:px-8">
+    <div className="mt-[64px] flex min-h-[calc(100vh-64px)] items-center justify-center bg-[rgba(200,248,253,0.33)] py-12 sm:px-6 lg:px-8">
       {children}
     </div>
   );
