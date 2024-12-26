c29549e0eeb43394fa3d71bea3f7f97de024b61b
yyezzzy <t47047877@gmail.com>
2024-12-26 15:11:30 +0900

    chore: signup bg color fix

--- src/app/components/layout/auth/Background.tsx
+++ src/app/components/layout/auth/Background.tsx
@@ -1,6 +1,6 @@
 export default function Background({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mt-[64px] flex min-h-[calc(100vh-64px)] items-center justify-center bg-[rgba(200,248,253,0.33)] py-12 sm:px-6 lg:px-8">
+    <div className="mt-[64px] flex min-h-[calc(100vh-64px)] items-center justify-center bg-primary-blue-50 py-12 sm:px-6 lg:px-8">
       {children}
     </div>
   );
